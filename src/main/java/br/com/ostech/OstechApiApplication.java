93710b12b87ca9c93f688b864c51bccb2ac8a4aa
Rafael <rafaajuda@hotmail.com>
2023-12-26 21:14:12 -0300

    first commit

---
+++ src/main/java/br/com/ostech/OstechApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ostech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OstechApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OstechApiApplication.class, args);
+	}
+
+}
